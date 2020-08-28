f2b7c112eff4dcceb967c6dbdb4522a0e8f04ca1
Benjamin Kyhn <benkyhn@hotmail.com>
2020-08-28 14:41:56 +0200

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(80);
+            System.out.println("Server started and listening on port 80");
+            Socket socket = serverSocket.accept();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream());
+            BufferedOutputStream dataOut = new BufferedOutputStream(socket.getOutputStream());
+
+            while (true){
+                String input = in.readLine();
+                System.out.println(input);
+                if (input.isEmpty()){
+                    System.out.println("Request received");
+                    break;
+                }
+            }
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
