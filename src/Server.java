bcc6e456ea988613d38e91754c149a4d2eb8ceb7
Benjamin Kyhn <benkyhn@hotmail.com>
2020-09-03 11:48:50 +0200

    Server sends html code

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class Server {
     public static void main(String[] args) {
@@ -9,23 +10,37 @@ public class Server {
             System.out.println("Server started and listening on port 80");
             Socket socket = serverSocket.accept();
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream());
-            BufferedOutputStream dataOut = new BufferedOutputStream(socket.getOutputStream());
+            BufferedReader request = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter responseHeaders = new PrintWriter(socket.getOutputStream());
+            DataOutputStream responseStream = new DataOutputStream(socket.getOutputStream());
 
             while (true){
-                String input = in.readLine();
+                String input = request.readLine();
                 System.out.println(input);
                 if (input.isEmpty()){
                     System.out.println("Request received");
-                    out.println("HTTP/1.1 200 OK");
-                    out.println();
-                    out.flush();
-                    //dataOut.write();
+                    responseHeaders.println("HTTP/1.1 200 OK");
+                    responseHeaders.println("Content-Type: text/html; charset=utf-8");
+                    responseHeaders.println();
+                    responseHeaders.flush();
+
+                    responseStream.writeUTF("<h1>En webserver</h1><br>");
+                    responseStream.writeUTF("<h2>En overskrift</h2>");
+                    responseStream.writeUTF("<p>Et afsnit.</p>");
+                    responseStream.writeUTF("<img src=\"https://i.imgur.com/40lws61.jpg\">");
+                    responseStream.flush();
+
+                    responseHeaders.close();
+                    responseStream.close();
+                    System.out.println("Sent to the browser");
                     break;
                 }
             }
-        } catch (IOException ex){
+        }
+        catch (SocketException e){
+            System.out.println("Connection closed after successful response-request");
+        }
+        catch (IOException ex){
             ex.printStackTrace();
         }
     }
